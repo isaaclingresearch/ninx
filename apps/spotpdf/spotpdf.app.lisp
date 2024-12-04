65c8279d814c691b205cb02be0b8a9e210beb467
muswawir <lam@ninx.xyz>
2024-12-04 09:39:57 +0300

    add indexnow

--- apps/spotpdf/spotpdf.app.lisp
+++ apps/spotpdf/spotpdf.app.lisp
@@ -465,6 +465,14 @@
   (setf (header-out "content-disposition") "inline; filename=favicon.ico")
   (ninx:read-binary-file-to-octets #p"~/common-lisp/ninx/priv/spotpdf/static/icons/web/favicon.ico"))
 
+;; this sends the index now file
+(define-easy-handler (index-now
+		      :uri (define-matching-functions "/94b5eaed6c084ba9a52bdae3fad2e459.txt" *spotpdf-host*)
+		      :host *spotpdf-host*) ()
+  (setf (content-type*) "text/plain")
+  (setf (header-out "content-disposition" "inline; filename=94b5eaed6c084ba9a52bdae3fad2e459.txt"))
+  (ninx:read-binary-file-to-octets #p"~/common-lisp/ninx/priv/spotpdf/94b5eaed6c084ba9a52bdae3fad2e459.txt"))
+
 ;; (trivia:match (get-downloadable-data dir)
 ;;   ((list type file-name data)
 ;;    (setf (content-type*) type)
