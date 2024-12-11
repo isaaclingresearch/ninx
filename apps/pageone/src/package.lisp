c9e90a44cf5e74ed6cb8eae4f37c617c6c626d59
muswawir <lam@ninx.xyz>
2024-12-11 14:11:48 +0300

    add header to app

--- apps/pageone/src/package.lisp
+++ apps/pageone/src/package.lisp
@@ -10,4 +10,3 @@
   (:documentation "This is the main backend package for the Frontpage application.")
   (:local-nicknames (:jzon :com.inuoe.jzon) (:ppcre :cl-ppcre))
   (:export :start-frontpage :stop-front-page :restart-frontpage))
-
