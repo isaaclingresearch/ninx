7dc26a66951b6557d1bcb3fa9bb0721c5c25718c
muswawir <lam@ninx.xyz>
2025-01-08 21:12:49 +0300

    migrate cenna

--- apps/cenna/doc/strategy.md
+++
@@ -1,3 +0,0 @@
-Providing a medical doctor to everyone on earth.
-
-We are going to need a magic trick to pull it off.
--- apps/cenna/src/cenna.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :cenna)
-
-(defvar *cenna-url* (uiop:getenv "CENNA_HOST"))
-(defvar *cenna-host* (let ((host (uiop:getenv "CENNA_HOST")))
-			 (if (equal "443" ninx:*ninx-https-port*)
-			     host
-			     (format nil "~a:~a" host ninx:*ninx-https-port*))))
